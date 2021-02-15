9e53017e40e149127c0b907b29314a6764c598b4
Thomas Göttsching <support@thomasgoettsching.de>
2021-02-15 13:10:02 +0100

    - Add Current User to Header - Add Changelog

--- resource/backend/tgsoft/modules/main/functions/webserver.js
+++ resource/backend/tgsoft/modules/main/functions/webserver.js
@@ -1,6 +1,23 @@
 import { TGSoft } from "../../tgsoft/tgsoft.js";
+import fs from 'fs';
+import DOMParser from 'dom-parser';
 
 /** WebServer -> Backend MainPage -> Get **/
 TGSoft.webServer.app.get('/backend', TGSoft.webServer.checkAuthenticated_Backend, (req, res) => {
-    TGSoft.webServer.toOutput(req, res, ['tgsoft', 'modules', 'main'], 'index.twig', { });
+
+    let filename = TGSoft.directories.root + TGSoft.settings.main.changelogFile;
+
+    let params = {
+        changelog: '',
+    }
+
+    fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) { throw err; }
+        else {
+            let parser = new DOMParser();
+            params.changelog = (parser.parseFromString(data, 'text/html')).rawHTML;
+            params.changelog =params.changelog.replace(/\r\n/g, '<br/>');
+        }
+        TGSoft.webServer.toOutput(req, res, ['tgsoft', 'modules', 'main'], 'index.twig', params);
+    });
 })
